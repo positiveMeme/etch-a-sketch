bebbaee88f316a7b0ffaaefaeafcf66ef75a4c96
positiveMeme <joe.arthurs@hotmail.co.uk>
2022-11-19 16:23:16 -0500

    created game, basic layout, functions. Need to add buttons and grid size selector

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # etch-a-sketch
 
-To make a simple etch-a-sketch game with html js and css
-To look attractive, have colours, be user-friendly
+To make a simple etch-a-sketch game with html js and css. 
+To look attractive, have colours, be user-friendly.
--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-alert('hello');
+
+const sides = 20;
+
+
+for (let i = 0; i < sides * sides; i++) {
+    const game = document.querySelector('.game')
+    const div = document.createElement('div');
+    game.appendChild(div);
+    div.classList.add("squares");
+    game.style.gridTemplateColumns = columns(sides);
+    div.addEventListener("mouseover", event => {
+        div.style.backgroundColor = color();
+    });
+}
+
+function columns(number) {
+    let cols = '';
+    for (let i = 0; i < number; i++) {
+        let auto = 'auto ';
+        cols = cols.concat(auto);
+    }
+    return cols;
+};
+
+function getColorCode() {
+    var makeColorCode = '0123456789ABCDEF';
+    var code = '#';
+    for (let count = 0; count < 6; count++) {
+        code = code + makeColorCode[Math.floor(Math.random() * 16)];
+    }
+    return code;
+}
+
+function color() {
+    return getColorCode();
+}
+const button = document.getElementById('btn');
+button.addEventListener("click",);
