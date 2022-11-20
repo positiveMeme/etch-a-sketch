55cc117cb9c497a7ce41221ef083a5973aa64564
positiveMeme <joe.arthurs@hotmail.co.uk>
2022-11-20 14:44:42 -0500

    game functioning, not added change grid popup or greyscale

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 
-const sides = 20;
+let sides = 30;
+const buttons = document.querySelectorAll('.options');
+const newGridButton = document.querySelector('#new-grid')
 
+// newGridButton.addEventListener("click", () => {
+//     let input = prompt("Choose number of sides (2-100)");
+//     return sides = parseInt(input);
+// });
+
+
+for (let button of buttons) {
+    button.addEventListener('click', function () {
+        for (let button of buttons) {
+            button.classList.remove('checked');
+        }
+        button.classList.toggle('checked');
+    })
+};
 
 for (let i = 0; i < sides * sides; i++) {
     const game = document.querySelector('.game')
@@ -8,10 +24,16 @@ for (let i = 0; i < sides * sides; i++) {
     game.appendChild(div);
     div.classList.add("squares");
     game.style.gridTemplateColumns = columns(sides);
-    div.addEventListener("mouseover", event => {
+    div.addEventListener("mouseover", () => {
         div.style.backgroundColor = color();
     });
-}
+    const clearButton = document.querySelector('.clear')
+    clearButton.addEventListener("click", () => {
+        div.style.backgroundColor = 'white';
+    });
+};
+
+
 
 function columns(number) {
     let cols = '';
@@ -23,20 +45,41 @@ function columns(number) {
 };
 
 function getColorCode() {
-    var makeColorCode = '0123456789ABCDEF';
-    var code = '#';
+    const makeColorCode = '0123456789ABCDEF';
+    let code = '#';
     for (let count = 0; count < 6; count++) {
         code = code + makeColorCode[Math.floor(Math.random() * 16)];
     }
     return code;
 };
 
+function singleColor() {
+    return 'black';
+};
+
+// function greyscale() {
+
+// }
+
+function erase() {
+    return 'white';
+};
+
+function clear() {
+    const game = document.querySelector('.game');
+    game.style.backgroundColor = 'white';
+};
+
 function color() {
-    return getColorCode();
-}
-
-const buttons = document.querySelectorAll('button');
-
-for (i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () { alert("Hello World!"); });
-}
+    const col = document.querySelector('#col-btn');
+    const rbw = document.querySelector('#rbw-btn');
+    const ers = document.querySelector('#ers-btn');
+    const clr = document.querySelector('#clr-btn');
+    if (col.classList.contains('checked') === true) {
+        return singleColor();
+    }
+    if (rbw.classList.contains('checked') === true) {
+        return getColorCode();
+    }
+    return erase();
+};
